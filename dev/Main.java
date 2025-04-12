f4089b1c57c90dfc2dcf3bb2dce2040950ada811
Sagi Katan <sagikatan9@gmail.com>
2025-04-12 14:19:38 +0300

    create document class

--- dev/Main.java
+++ dev/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+import transport_module.*;
+
 public class Main {
+    public static void menu_message(){
+        System.out.println("Welcome to the Transportation Department!");
+        System.out.println("Please choose your next");
+        System.out.println("1. Add new transport");
+        System.out.println("2. Print all Trucks");
+        System.out.println("3. Print all Drivers");
+        System.out.println("3. Print all Drivers");
+
+
+    }
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            menu_message();
+
+        }
 
     }
 
