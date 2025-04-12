357ce4d811356df3dce6bd66e4dc9c899c7573bc
Sagi Katan <sagikatan9@gmail.com>
2025-04-12 14:27:27 +0300

    write class Document

--- dev/transport_module/Document.java
+++ dev/transport_module/Document.java
@@ -1,4 +1,33 @@
 package transport_module;
 
+import java.util.ArrayList;
+
 public class Document {
+    public static int documentID = 0;
+    private int id;
+    private Site destination;
+    private ArrayList<Product> productsList;
+
+    public Document(Site site) throws Exception{
+        if (site ==null) throw new Exception("InValid Input");
+        id=++documentID;
+        destination = new Site(site);
+        productsList = new ArrayList<>();
+    }
+    public void add_product(Product p) throws Exception{
+        if (p == null) throw new Exception("Invalid Product");
+        productsList.add(new Product (p));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Site getDestination() {
+        return destination;
+    }
+
+    public ArrayList<Product> getProductsList() {
+        return productsList;
+    }
 }
