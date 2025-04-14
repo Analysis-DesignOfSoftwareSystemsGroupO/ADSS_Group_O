db1e410f5ec035b3279b0873bfe320c0ffbfa591
Sagi Katan <sagikatan9@gmail.com>
2025-04-14 15:54:09 +0300

    update truck by omer branch

--- dev/transport_module/Truck.java
+++ dev/transport_module/Truck.java
@@ -1,18 +1,50 @@
 package transport_module;
+
+import java.util.HashMap;
+
 // import Pritim
+
+/**
+ * multiTone class, holsds all it's instances in a static HashMap. To create an instance outside the class, has to use instance()
+ */
 public class Truck {
+    static private HashMap<String, Truck> trucks;
     private int weight;
     private int maxWeight;
     private boolean available;
     private DrivingLicence liceenceReq;
     private String plateNumber;
 
-    public Truck(DrivingLicence dl, int maxWeight, String pn ){
+
+    private Truck(DrivingLicence dl, int maxWeight, String pn ){
         this.maxWeight = maxWeight;
         this.liceenceReq = dl;
         this.available =true; // inited as true
         this.plateNumber = pn;
     }
+
+    /**
+     * If a truck with the pn already exsists, return this truck. If not create a new truck based on the arguments and store it in the HAsh map.
+     * @param dl Driving Licence needed for the truck
+     * @param maxWeight max weight of the truck
+     * @param pn Unique plate number of the truck
+     * @return truck instance
+     */
+    public Truck instance( DrivingLicence dl, int maxWeight, String pn ){
+        if(trucks.containsKey(pn)){ // trucks already exsist in the System, returns it
+            return trucks.get(pn);
+        }
+        Truck t = new Truck( dl,  maxWeight, pn ); //create a new truck
+        trucks.put(pn, t); // add the new truck to the HashMap
+        return t; //return the new truck
+    }
+
+    /**
+     *
+     * @return the static map of all trucks.
+     */
+    public static HashMap<String, Truck> getTrucks(){return trucks;}
+
     /**
      * @return weight of the truck
      */
@@ -54,6 +86,7 @@ public class Truck {
         }
         return false;
     }
+
     @Override
     public String toString(){
         StringBuilder str = new StringBuilder(); // build new string
