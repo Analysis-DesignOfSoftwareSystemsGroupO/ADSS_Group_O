c2af9b59415d062f29f1fe94f415314cd650374e
Sagi Katan <sagikatan9@gmail.com>
2025-05-10 14:26:29 +0300

    write truck manager menu

--- dev/Users/ITruckManager.java
+++ dev/Users/ITruckManager.java
@@ -1,50 +1,86 @@
 package Users;
+
 import Transport_Module_Exceptions.ATransportModuleException;
+import Transport_Module_Exceptions.InvalidDateFormatException;
 import Transport_Module_Exceptions.InvalidInputException;
 import transport_module.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Objects;
+import java.util.Map;
 import java.util.Scanner;
 
 public interface ITruckManager {
 
-    default void addNewTruckToSystem(Scanner scanner, Truck[] trucks,DrivingLicence[] drivingLicence) throws ATransportModuleException {
-        if(trucks == null || scanner == null || drivingLicence == null)
+
+    /***
+     * function that add new truck to system
+     * @param scanner input variable
+     * @param trucks map of trucks and their plate numbers
+     * @param drivingLicences map of all driving licences in system
+     * @throws ATransportModuleException if input is invalid / truck is already in system / driving license is not in system
+     */
+    default void addNewTruckToSystem(Scanner scanner, Map<String, Truck> trucks, Map<String, DrivingLicence> drivingLicences) throws ATransportModuleException {
+        if (trucks == null || scanner == null || drivingLicences == null)
             throw new InvalidInputException();
         System.out.println("Please enter Truck's plate number");
         String platenumber = scanner.nextLine();
-        for (Truck truck: trucks){
-            if (Objects.equals(truck.getPlateNumber(), platenumber))
-                throw new InvalidInputException("Truck is already in system");
-        }
+        if (trucks.get(platenumber) != null)
+            throw new InvalidInputException("Truck is already in system");
         System.out.println("Please enter driving licence code");
         String drivinglincenceCode = scanner.nextLine();
+        DrivingLicence drivingLicence = drivingLicences.get(drivinglincenceCode);
+        if (drivingLicence == null)
+            throw new InvalidInputException("driving licence is not in system");
+        int maxweight;
+        try {
+            System.out.println("Please enter maximum");
+            maxweight = scanner.nextInt();
+        } catch (Exception e) {
+            throw new InvalidInputException("Please enter integer number");
+        }
+        if (maxweight <= 0) {
+            throw new InvalidInputException("Please enter positive number");
+        }
+        Truck t = new Truck(drivingLicence, maxweight, platenumber);
+        trucks.put(platenumber, t);
+        System.out.println("Truck has successfully added");
+
 
     }
-    default void printAllTrucks(Truck[] trucks) throws ATransportModuleException{
-            if (trucks == null)
-                throw new InvalidInputException("Trucks array is null");
-            for(Truck truck : trucks){
-                System.out.println(truck);
-            }
+
+    /***
+     * function that add prints all trucks in system
+     * @param trucks map of trucks and their plate numbers
+     * @throws ATransportModuleException if input is invalid */
+    default void printAllTrucks(Map<String, Truck> trucks) throws ATransportModuleException {
+        if (trucks == null)
+            throw new InvalidInputException("Trucks data is null");
+        for (String platenumber : trucks.keySet()) {
+            System.out.println(trucks.get(platenumber));
+        }
     }
-    default void  printAllAvailableTrukcs(Truck[] trucks, Scanner scanner){
+
+    /***
+     * function that add prints all available trucks in specific date
+     * @param trucks map of trucks and their plate numbers
+     * @throws ATransportModuleException if input is invalid */
+    default void printAllAvailableTrukcs(Map<String, Truck> trucks, Scanner scanner) throws ATransportModuleException {
+        if (trucks == null || scanner == null)
+            throw new InvalidInputException();
         System.out.println("Please enter date by format: DD/MM/YYYY"); // ask for date from user
         String dateStr = scanner.nextLine();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // try to create date variable
         LocalDate parsedDate;
         try { // if date format is wrong
             parsedDate = LocalDate.parse(dateStr, formatter);
-        }
-        catch (DateTimeParseException e){
-            System.out.println("Invalid date format - please try again with the format: DD/MM/YYYY"); // message to user and ask him to try again
-            return;
+        } catch (DateTimeParseException e) {
+            throw new InvalidDateFormatException("Invalid date format - please try again with the format: DD/MM/YYYY");
         }
         int count = 0; // count all avalialbe trucks in system
-        for (Truck truck : trucks) { // for each truck in database
+        for (String platenumber : trucks.keySet()) { // for each truck in database
+            Truck truck = trucks.get(platenumber);
             if (truck.getAvailablity(parsedDate)) { // if truck is available at this date
                 System.out.println(truck); // print its data
                 count++; // count how many trucks are available
@@ -56,11 +92,24 @@ public interface ITruckManager {
 
     }
 
-    default void removeTruckFromSystem(Truck truck, Truck[] trucks){
+    /***
+     * function that add prints all available trucks in specific date
+     * @param trucks map of trucks and their plate numbers
+     * @throws ATransportModuleException if input is invalid  / truck is not in system*/
+    default void removeTruckFromSystem(Map<String, Truck> trucks, Scanner scanner) throws ATransportModuleException {
+        if (trucks == null || scanner == null)
+            throw new InvalidInputException();
+        System.out.println("Please enter Truck's plate number you want to remove");
+        String platenumber = scanner.nextLine();
+        Truck truck = trucks.get(platenumber);
+        if (truck == null)
+            throw new InvalidInputException("Truck is not in system");
+        trucks.remove(platenumber);
+        System.out.println("Truck has successfully removed");
 
     }
 
-    default void TruckManagerMenuMessage(){
+    default void TruckManagerMenuMessage() {
         System.out.println("Welcome to Truck Manager Menu!");
         System.out.println("Please choose your next step");
         System.out.println("1. Add new Truck");
@@ -69,6 +118,57 @@ public interface ITruckManager {
         System.out.println("4. Remove Truck from System");
 
         System.out.println("Press E to Exit.");
+    }
+
+    default void TruckManagerMenu(Map<String, Truck> trucks, Map<String, DrivingLicence> drivingLicences) throws ATransportModuleException {
+        if (trucks == null || drivingLicences == null)
+            throw new InvalidInputException();
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            TruckManagerMenuMessage();
+            String input = scanner.nextLine();
+            try {
+
+
+                switch (input) {
+                    case "1": {
+                        addNewTruckToSystem(scanner, trucks, drivingLicences);
+                        break;
+                    }
+                    case "2": {
+                        printAllTrucks(trucks);
+                        break;
+                    }
+                    case "3":{
+                        printAllAvailableTrukcs(trucks,scanner);
+                        break;
+                    }
+                    case "4":
+                    {
+                        removeTruckFromSystem(trucks,scanner);
+                        break;
+                    }
+                    case "E","e":
+                    {
+                        running = false;
+                        System.out.println("Thank you for using Trucks menu");
+                        break;
+                    }
+                    default:
+                    {
+                        System.out.println("Wrong input! Please try again.");
+                        break;
+                    }
+
+
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+
+            }
+        }
     }
 
 
