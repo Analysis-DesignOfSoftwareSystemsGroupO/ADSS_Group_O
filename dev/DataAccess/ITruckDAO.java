b7f751455bff3b604b4000cc4c37248e85f19842
OmerSherman <omersherman22@gmail.com>
2025-05-26 16:06:33 +0300

    Added assigntruckToDAte and checkTruckAvailability to TruckDAO

--- dev/DataAccess/ITruckDAO.java
+++ dev/DataAccess/ITruckDAO.java
@@ -1,8 +1,10 @@
 package DataAccess;
 
 import DTO.TruckDto;
+import Transport_Module_Exceptions.UnAvailableTruckException;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +14,6 @@ public interface ITruckDAO {
     Optional<TruckDto> findByTruckPN(String pn) throws SQLException;
     List<TruckDto> findAllTrucks() throws SQLException;
     void deleteTruck(String pn) throws SQLException;
+    boolean checkAvailabilityOfTruck(String truckPN, LocalDate date)throws SQLException;
+    void assignTruckToDate(String truckPN, LocalDate date)throws SQLException, UnAvailableTruckException;
 }
