60e5175aecde94dbecbe75becc819b4fecccb090
shay7210 <shay7210@gmail.com>
2025-05-27 16:24:31 +0300

    finished dao classes and rewrote loadata to load the data to the database

--- dev/SupplierMoudleSource/DAO/BranchDAO.java
+++ dev/SupplierMoudleSource/DAO/BranchDAO.java
@@ -4,6 +4,7 @@ import SupplierMoudleSource.Domain.Branch;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static DataBase.PostgresConnection.getConnection;
@@ -14,6 +15,9 @@ public class BranchDAO {
     }
 
     public void addBranch(String branchCity, String branchAddress) throws SQLException {
+        if (isBranchExist(branchCity, branchAddress)) {
+            return;
+        }
         String sql = "INSERT INTO supplierinventorydb.branch (city, address) VALUES (?, ?)";
 
         try (Connection con = getConnection();
@@ -23,6 +27,21 @@ public class BranchDAO {
             pstmt.setString(2, branchAddress);
             pstmt.executeUpdate();
         }
+    }
+
+    private boolean isBranchExist(String branchCity, String branchAddress) throws SQLException {
+        String sql = "Select * from supplierinventorydb.branch where city = ? and address = ?";
+        try (Connection con = getConnection()){
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, branchCity);
+            pstmt.setString(2, branchAddress);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+            return false;
+        }
+
     }
 
 
