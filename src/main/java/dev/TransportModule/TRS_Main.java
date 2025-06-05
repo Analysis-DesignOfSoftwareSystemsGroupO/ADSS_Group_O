5b9f2d19d0be288babb0feec919805307c739112
Sagi Katan <sagikatan9@gmail.com>
2025-06-05 09:29:16 +0300

    refactor dev to src because of maven project

--- src/main/java/dev/TransportModule/TRS_Main.java
+++ src/main/java/dev/TransportModule/TRS_Main.java
@@ -20,7 +20,7 @@ public class TRS_Main {
 
 
 
-    public static void main(String[] args) {
+    public static void TRS_main(String[] args) {
 
         Map<String, User> users = new HashMap<>();
         try {
