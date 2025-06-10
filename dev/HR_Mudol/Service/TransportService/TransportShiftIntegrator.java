31cf072976e3f99b709a9e8a904ec4e85379a639
avrshay <shaymilo23@gmail.com>
2025-06-10 18:59:59 +0300

    last commit 10/6 19:00

--- dev/HR_Mudol/Service/TransportService/TransportShiftIntegrator.java
+++ dev/HR_Mudol/Service/TransportService/TransportShiftIntegrator.java
@@ -51,7 +51,7 @@ public class TransportShiftIntegrator implements ITransportShiftIntegrator {
                     String branchName=hrService.getBranchOfShift(shiftDTO);
                     String siteStart=transport.getSiteName();
                     if (licence != null && !licence.trim().isEmpty() && branchName.trim().equalsIgnoreCase(siteStart.trim())) {//בדיקה למקרה שלא שובצה משאית להובלה + בדיקה שהסניף רלוונטי
-                        licence="Driver "+licence+ ":" + transport.getId();
+                        licence="Driver "+licence;
                         hrService.insertNewRole(licence);
 
                         driverDTO = getRoleByDescription(licence, theCaller);
--- dev/HR_Mudol/domain/Objects/Shift.java
+++ dev/HR_Mudol/domain/Objects/Shift.java
@@ -87,11 +87,6 @@ public class Shift {
         if (r == null) {
             throw new IllegalArgumentException("Role is null.");
         }
-        /*
-        if (necessaryRoles.contains(r)) {
-            throw new IllegalArgumentException("Role already exists in the shift.");
-        }
-        */
 
         this.necessaryRoles.add(r);
     }
